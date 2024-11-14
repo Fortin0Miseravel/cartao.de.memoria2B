b29c65d619770df164189aae9510b7f6d8440aa8
Leonardo <142313648+Fortin0Miseravel@users.noreply.github.com>
2024-11-14 07:35:43 -0300

    aula32854t

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'One Piece',
+    ' Oque é uma kokuto?',
+    'São espadas que com o passar do tempo ficam com lâminas negras'
+
+)
+criaCartao(
+    'One Piece',
+    'Como as akumas-no-mi surgem segundo Vegapunk?',
+    'Através do deejo das pessoa ela se materializam em forma de frutas'
+    
+)
+criaCartao(
+    'One Piece',
+    'Qual foi o único membro os chapéus de palha que mentiu ou enganou sua tripulação?',
+    'Nami'
+    
+)
